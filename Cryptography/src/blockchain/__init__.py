267500e3c263bcd59eaf5dac11286fdaadc9bc07
ADIL <138507177+KIAMOOO@users.noreply.github.com>
2025-12-22 11:06:23 +0500

    Delete Cryptography directory

--- Cryptography/src/blockchain/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Blockchain audit ledger module for immutable logging
-"""
-
-from .blockchain import BlockchainModule, Block, Transaction
-
-__all__ = ['BlockchainModule', 'Block', 'Transaction']
-
--- Cryptography/src/files/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-File encryption module for secure file storage
-"""
-
-from .file_encryption import FileEncryptionModule
-
-__all__ = ['FileEncryptionModule']
-
